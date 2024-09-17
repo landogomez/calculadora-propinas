4a981e67a171a7cfcafe24d3a848c8870a2a9895
jorge <esgerry81@gmail.com>
2024-09-16 23:13:58 -0600

    Añadiendo elementos al state Orden

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -2,13 +2,13 @@ import type { MenuItem } from "../types"
 
 type MenuItemProps = {
     item: MenuItem,
-    addItem: () => void
+    addItem: (item: MenuItem) => void
 }
 export default function MenuItem({item, addItem} : MenuItemProps) {
   return (
     <button
     className="border-2 border-teal-400 hover:bg-teal-100 w-full p-3 flex justify-between"
-    onClick={() => addItem()}
+    onClick={() => addItem(item)}
     
     >
 
--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,11 +1,13 @@
 import { useState } from "react"
-import type { OrderItem } from "../types"
+import type { MenuItem, OrderItem } from "../types"
 
 export default function useOrder() {
     const [order, setOrder] = useState<OrderItem[]>([])
 
-    const addItem = () => {
-        console.log('Agregando...')
+    const addItem = (item: MenuItem) => {
+        
+        const newItem = {...item, quantity: 1}
+        setOrder([...order, newItem])
     }
 
 
