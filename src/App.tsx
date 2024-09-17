8a0805e659171440fb8b8dd7e329c9749660c420
jorge <esgerry81@gmail.com>
2024-09-17 13:14:50 -0600

    Se implemento la funcion que calcula el subtotal a pagar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import MenuItem from "./components/MenuItem"
 import OrderContents from "./components/OrderContents"
+import OrderTotals from "./components/OrderTotals"
 import { menuItems } from "./data/db"
 import useOrder from "./hooks/useOrder"
 
@@ -36,6 +37,12 @@ function App() {
           <OrderContents 
             order = {order}
             removeItem = {removeItem}
+          
+          
+          />
+
+          <OrderTotals 
+            order = {order}
           
           
           />
