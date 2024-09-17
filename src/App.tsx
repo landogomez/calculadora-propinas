5c64765bcdc3f76645b4c2be4e3c923732c8de8a
jorge <esgerry81@gmail.com>
2024-09-17 12:21:03 -0600

    Mostrando el consumo de la orden

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import MenuItem from "./components/MenuItem"
+import OrderContents from "./components/OrderContents"
 import { menuItems } from "./data/db"
 import useOrder from "./hooks/useOrder"
 
@@ -6,7 +7,7 @@ import useOrder from "./hooks/useOrder"
 
 function App() {
 
-  const { addItem } = useOrder()
+  const { order,  addItem } = useOrder()
 
   return (
     <>
@@ -31,8 +32,12 @@ function App() {
 
         </div>
 
-        <div>
-          <h2>Consumo</h2>
+        <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
+          <OrderContents 
+            order = {order}
+          
+          
+          />
         </div>
 
       </main>
--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -19,10 +19,10 @@ export default function useOrder() {
         }
     }
 
-    console.log(order)
-
 
     return {
+        order,
         addItem
+        
     }
 }
